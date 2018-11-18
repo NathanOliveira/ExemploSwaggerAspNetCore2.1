26bdfbdfc0375f77615cf0eabff51fdf1ea7e5e3
Nathan Justino de Oliveira <nathan.oliveira@bgmrodotec.com.br>
2018-11-18 04:20:37 -0200

    Add project files.

---
+++ ExemploSwaggerAspNetCore2.1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Authorization;
+
+//namespace ExemploSwaggerAspNetCore2._1.Controllers
+//{
+//    /// <summary>
+//    /// </summary>
+//    [AllowAnonymous]
+//    public class HomeController : Controller
+//    {
+//        /// <summary>
+//        /// 
+//        /// </summary>
+//        /// <returns></returns>
+//        public ActionResult Index() => new RedirectResult("~/swagger");
+//    }
+//}
